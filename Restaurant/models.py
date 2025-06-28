cf155f8c53d032c7c61004f9cedbcfbc54592ad7
ankit kushwaha <theanonymoush4ck3r@gmail.com>
2025-06-28 16:19:24 +0530

    update some more features

--- Restaurant/models.py
+++ Restaurant/models.py
@@ -50,7 +50,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/',default="/menu_items/set-vector.jpg")
+    image = models.ImageField(upload_to='menu_images/',default="menu_items/set-vector.jpg")
     delivery_time = models.CharField(max_length=15, null=True, default="20 min")
     slug = AutoSlugField(populate_from='name', unique=True, always_update=False)
 
